54de39db88d3cd5e3ee43365f5ab0c1ff66f9959
KameriiJ <jenny.sirarat@gmail.com>
2018-05-06 16:59:06 +0700

    fixed problems

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
