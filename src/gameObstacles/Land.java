4b94c7f7678cc852795aed1ca6d027532af94d2b
KameriiJ <jenny.sirarat@gmail.com>
2018-05-07 15:44:00 +0700

    fixed problem

--- src/gameObstacles/Land.java
+++ src/gameObstacles/Land.java
@@ -2,7 +2,6 @@ package gameObstacles;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
